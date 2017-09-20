6d01ec0fb5ab712838beffe77d971b8839d0d78d
ana.alves <ana.alves@SQEST075.squadra.com.br>
2017-09-20 14:46:02 -0300

    fix: template

--- pokemon/src/main/java/br/com/ana/alves/araujo/pokemon/config/JpaConfiguration.java
+++ pokemon/src/main/java/br/com/ana/alves/araujo/pokemon/config/JpaConfiguration.java
@@ -27,90 +27,90 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
-@EnableJpaRepositories(basePackages = "br.com.ana.alves.araujo.pokemon.repositories", entityManagerFactoryRef = "entityManagerFactory", transactionManagerRef = "transactionManager")
+@EnableJpaRepositories(basePackages = "br.com.ana.alves.araujo.pokemon.repositories",
+        entityManagerFactoryRef = "entityManagerFactory", transactionManagerRef = "transactionManager")
 @EnableTransactionManagement
 public class JpaConfiguration {
 
-	@Autowired
-	private Environment environment;
+    @Autowired
+    private Environment environment;
 
-	@Value("${datasource.sampleapp.maxPoolSize:10}")
-	private int maxPoolSize;
+    @Value("${datasource.sampleapp.maxPoolSize:10}")
+    private int maxPoolSize;
 
-	/*
-	 * Populate SpringBoot DataSourceProperties object directly from
-	 * application.yml based on prefix.Thanks to .yml, Hierachical data is
-	 * mapped out of the box with matching-name properties of
-	 * DataSourceProperties object].
-	 */
-	@Bean
-	@Primary
-	@ConfigurationProperties(prefix = "datasource.sampleapp")
-	public DataSourceProperties dataSourceProperties() {
-		return new DataSourceProperties();
-	}
+    /*
+     * Populate SpringBoot DataSourceProperties object directly from
+     * application.yml based on prefix.Thanks to .yml, Hierachical data is
+     * mapped out of the box with matching-name properties of
+     * DataSourceProperties object].
+     */
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "datasource.sampleapp")
+    public DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
 
-	/*
-	 * Configure HikariCP pooled DataSource.
-	 */
-	@Bean
-	public DataSource dataSource() {
-		DataSourceProperties dataSourceProperties = dataSourceProperties();
-		HikariDataSource dataSource = (HikariDataSource) DataSourceBuilder.create(dataSourceProperties.getClassLoader())
-				.driverClassName(dataSourceProperties.getDriverClassName()).url(dataSourceProperties.getUrl())
-				.username(dataSourceProperties.getUsername()).password(dataSourceProperties.getPassword())
-				.type(HikariDataSource.class).build();
-		dataSource.setMaximumPoolSize(maxPoolSize);
-		return dataSource;
-	}
+    /*
+     * Configure HikariCP pooled DataSource.
+     */
+    @Bean
+    public DataSource dataSource() {
+        DataSourceProperties dataSourceProperties = dataSourceProperties();
+        HikariDataSource dataSource = (HikariDataSource) DataSourceBuilder.create(dataSourceProperties.getClassLoader())
+                .driverClassName(dataSourceProperties.getDriverClassName()).url(dataSourceProperties.getUrl())
+                .username(dataSourceProperties.getUsername()).password(dataSourceProperties.getPassword())
+                .type(HikariDataSource.class).build();
+        dataSource.setMaximumPoolSize(maxPoolSize);
+        return dataSource;
+    }
 
-	/*
-	 * Entity Manager Factory setup.
-	 */
-	@Bean
-	public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws NamingException {
-		LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
-		factoryBean.setDataSource(dataSource());
-		factoryBean.setPackagesToScan(new String[] { "br.com.ana.alves.araujo.pokemon.model" });
-		factoryBean.setJpaVendorAdapter(jpaVendorAdapter());
-		factoryBean.setJpaProperties(jpaProperties());
-		return factoryBean;
-	}
+    /*
+     * Entity Manager Factory setup.
+     */
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws NamingException {
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(dataSource());
+        factoryBean.setPackagesToScan(new String[] { "br.com.ana.alves.araujo.pokemon.model" });
+        factoryBean.setJpaVendorAdapter(jpaVendorAdapter());
+        factoryBean.setJpaProperties(jpaProperties());
+        return factoryBean;
+    }
 
-	/*
-	 * Provider specific adapter.
-	 */
-	@Bean
-	public JpaVendorAdapter jpaVendorAdapter() {
-		HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
-		return hibernateJpaVendorAdapter;
-	}
+    /*
+     * Provider specific adapter.
+     */
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        return new HibernateJpaVendorAdapter();
+    }
 
-	/*
-	 * Here you can specify any provider specific properties.
-	 */
-	private Properties jpaProperties() {
-		Properties properties = new Properties();
-		properties.put("hibernate.dialect", environment.getRequiredProperty("datasource.sampleapp.hibernate.dialect"));
-		properties.put("hibernate.hbm2ddl.auto",
-				environment.getRequiredProperty("datasource.sampleapp.hibernate.hbm2ddl.method"));
-		properties.put("hibernate.show_sql",
-				environment.getRequiredProperty("datasource.sampleapp.hibernate.show_sql"));
-		properties.put("hibernate.format_sql",
-				environment.getRequiredProperty("datasource.sampleapp.hibernate.format_sql"));
-		if (StringUtils.isNotEmpty(environment.getRequiredProperty("datasource.sampleapp.defaultSchema"))) {
-			properties.put("hibernate.default_schema",
-					environment.getRequiredProperty("datasource.sampleapp.defaultSchema"));
-		}
-		return properties;
-	}
+    /*
+     * Here you can specify any provider specific properties.
+     */
+    private Properties jpaProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", environment.getRequiredProperty("datasource.sampleapp.hibernate.dialect"));
+        properties.put("hibernate.hbm2ddl.auto",
+                environment.getRequiredProperty("datasource.sampleapp.hibernate.hbm2ddl.method"));
+        properties.put("hibernate.show_sql",
+                environment.getRequiredProperty("datasource.sampleapp.hibernate.show_sql"));
+        properties.put("hibernate.format_sql",
+                environment.getRequiredProperty("datasource.sampleapp.hibernate.format_sql"));
+        if (StringUtils.isNotEmpty(environment.getRequiredProperty("datasource.sampleapp.defaultSchema"))) {
+            properties.put("hibernate.default_schema",
+                    environment.getRequiredProperty("datasource.sampleapp.defaultSchema"));
+        }
+        return properties;
+    }
 
-	@Bean
-	@Autowired
-	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
-		JpaTransactionManager txManager = new JpaTransactionManager();
-		txManager.setEntityManagerFactory(emf);
-		return txManager;
-	}
+    @Bean
+    @Autowired
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(emf);
+        return txManager;
+    }
 
 }
